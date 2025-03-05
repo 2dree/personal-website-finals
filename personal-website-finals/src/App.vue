079d263eaf8d4e24a7c92e8341ba71c310371b20
2dree <pjco3@student.apc.edu.ph>
2025-03-05 08:07:43 +0000

    try

--- personal-website-finals/src/App.vue
+++ personal-website-finals/src/App.vue
@@ -18,30 +18,16 @@ import TheWelcome from './components/TheWelcome.vue'
 </template>
 
 <style scoped>
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+}
 header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    background: #333;
+    color: white;
+    padding: 20px;
 }
 </style>
+
