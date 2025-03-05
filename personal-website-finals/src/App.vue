ac5471ab9b154a895b8a10fa98dd2ec52e2dcf28
2dree <pjco3@student.apc.edu.ph>
2025-03-05 13:18:58 +0000

    Hello

--- personal-website-finals/src/App.vue
+++ personal-website-finals/src/App.vue
@@ -96,15 +96,15 @@ app.mount('#app');
 <!-- Second Parallax Image with Portfolio Text -->
 <div class="bgimg-2 w3-display-container w3-opacity-min">
   <div class="w3-display-middle">
-    <span class="w3-xxlarge w3-text-white w3-wide">PORTFOLIO</span>
+    <span class="w3-xxlarge w3-text-white w3-wide">MY GALLERY</span>
   </div>
 </div>
 
 
 <!-- Container (Portfolio Section) -->
 <div class="w3-content w3-container w3-padding-64" id="portfolio">
-  <h3 class="w3-center">MY WORK</h3>
-  <p class="w3-center"><em>Here are some of my latest lorem work ipsum tipsum.<br> Click on the images to make them bigger</em></p><br>
+  <h3 class="w3-center">PHOTOS</h3>
+  <p class="w3-center"><em>Here are some of my Photo Gallery Dump.<br> Too random</em></p><br>
 </div>
 
 
@@ -112,98 +112,154 @@ app.mount('#app');
 <!-- Responsive Grid. Four columns on tablets, laptops and desktops. Will stack on mobile devices/small screens (100% width) -->
 <div class="w3-row-padding w3-center">
     <div class="w3-col m3">
-      <img src="" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="The mist over the mountains">
+      <img src="https://scontent.fmnl4-2.fna.fbcdn.net/v/t1.15752-9/481904531_645654731162380_5552048263239859323_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=9f807c&_nc_ohc=MpKJpjGU7NoQ7kNvgF-CtFf&_nc_oc=Adg2UpHbl_s67dsfQ-BPkxlbbXla8OhYt-5AVWAlqDH8yes00sZT-Ruz94Z7KsNdBWw&_nc_zt=23&_nc_ht=scontent.fmnl4-2.fna&oh=03_Q7cD1gGO61hWog8VRlmddu2fodDHDHMfqW5lJmTSKOlCLgnAlw&oe=67EFA0D2" 
+      style="width:100%" class="w3-hover-opacity" alt="Potcor x starbs?">
     </div>
 
     <div class="w3-col m3">
-      <img src="" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Coffee beans">
+      <img src="https://scontent.fmnl4-7.fna.fbcdn.net/v/t1.15752-9/482148352_1004765961551815_3650695014809342931_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=9f807c&_nc_ohc=CV_DFUH-CesQ7kNvgEFpr7m&_nc_oc=AdgOku1D1jpLx9_loJhUcthpeQxcwwL23zii1UsXU-CaGdq-QOQA6SRQosmARuGLS_s&_nc_zt=23&_nc_ht=scontent.fmnl4-7.fna&oh=03_Q7cD1gHikIrG8yNivvpExuaAMaTGc9VGJ1d9asDZqeB9zLEUZQ&oe=67EF98B3" 
+      style="width:100%" class="w3-hover-opacity" alt="Nanyang">
     </div>
 
     <div class="w3-col m3">
-      <img src="" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Bear closeup">
+      <img src="https://scontent.fmnl4-4.fna.fbcdn.net/v/t1.15752-9/481978343_497002893264923_3668156600164171348_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=9f807c&_nc_ohc=I0Und6z8sakQ7kNvgF31nac&_nc_oc=AdjxJ0Mobklt0Usf0dk7TjBKzOYudZ2iDuIDZdrZwwtNOsKyrZ2sMLeJNeC5akNJQ2A&_nc_zt=23&_nc_ht=scontent.fmnl4-4.fna&oh=03_Q7cD1gFRPhlDYJZZYYJS44lNu7K6bVDfta54iI8AjhW37mU6VA&oe=67EFD4EA" 
+      style="width:100%" class="w3-hover-opacity" alt="Boat House?">
     </div>
 
     <div class="w3-col m3">
-      <img src="" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Quiet ocean">
+      <img src="https://scontent.fmnl4-2.fna.fbcdn.net/v/t1.15752-9/482292757_1159591921958639_3012151894625487496_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=9f807c&_nc_ohc=tIOdvbptA6wQ7kNvgHxYBqS&_nc_oc=AdhftMvzD-B_JJ2jdppm6u14kUbKUSh8ju0Cyek0cHMVq6Tzpq8nqZqS-LCqE9GPLi8&_nc_zt=23&_nc_ht=scontent.fmnl4-2.fna&oh=03_Q7cD1gE45b2BKTj5M4Ybd_tU1Jx2NH3wnooxlICREoHcquGTRg&oe=67EFD19F" 
+      style="width:100%" class="w3-hover-opacity" alt="Quiet ocean">
     </div>
   </div>
 
   <div class="w3-row-padding w3-center w3-section">
     <div class="w3-col m3">
-      <img src="" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="The mist">
+      <img src="https://scontent.fmnl4-4.fna.fbcdn.net/v/t1.15752-9/481808341_1179266047200039_707161907799634328_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=9f807c&_nc_ohc=FqC9UNdRgqMQ7kNvgEeFRuL&_nc_oc=AdhFfAq7BPzWpBPeUYl5c6cuNixIJ7no98gKixbqifQlmj6gEG7bCFOn2JS6VZJTjWA&_nc_zt=23&_nc_ht=scontent.fmnl4-4.fna&oh=03_Q7cD1gGDMnBUvQoPSowiqaOVinadTBG3_EgUefgOMWle8ifIGQ&oe=67EFD2B3" 
+      style="width:100%" class="w3-hover-opacity" alt="Niki">
     </div>
 
     <div class="w3-col m3">
-      <img src="" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="My beloved typewriter">
+      <img src="https://scontent.fmnl4-1.fna.fbcdn.net/v/t1.15752-9/482147584_1316119556381501_7837411078638539732_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=9f807c&_nc_ohc=tPU-J9LpB94Q7kNvgGpweO1&_nc_oc=Adi3G6veVsgPcqtB3eBSuUxIMBl_tTu-fu4lAVcVZPBqZaCWdbI86FX0a_PPnKgaME0&_nc_zt=23&_nc_ht=scontent.fmnl4-1.fna&oh=03_Q7cD1gGxDfsNsV770KaOXF5mKziQO7oiMJWMwO_FfC9as6H3Fw&oe=67EFA77A" 
+      style="width:100%"  class="w3-hover-opacity" alt="mcdo?">
     </div>
 
     <div class="w3-col m3">
-      <img src="" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Empty ghost train">
+      <img src="https://scontent.fmnl4-2.fna.fbcdn.net/v/t1.15752-9/481901244_645946974513115_4659766834960010463_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=9f807c&_nc_ohc=uf6AiItJyc4Q7kNvgFGFbVv&_nc_oc=AdgBHcJQvpldtNAamtwWSbuWQ-gOHR48TBCeNHWlg3TNaOdV8xnSsWTqCbXJLKb38qY&_nc_zt=23&_nc_ht=scontent.fmnl4-2.fna&oh=03_Q7cD1gEdivszip-GG0Ypx32cEnN5LWWRWipHbbsm0coxbI2_cQ&oe=67EFBA4F" 
+      style="width:100%"  class="w3-hover-opacity" alt="F1">
     </div>
 
     <div class="w3-col m3">
-      <img src="" style="width:100%" onclick="onClick(this)" class="w3-hover-opacity" alt="Sailing">
+      <img src="https://scontent.fmnl4-6.fna.fbcdn.net/v/t1.15752-9/481517309_938179091816620_8936053091630437504_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=9f807c&_nc_ohc=5LYnrSaAyeIQ7kNvgEeiHXc&_nc_oc=Adi0nheILQkWleSrnkj5Px3UIIrL9Sxy5VK4cjG1K3-RkQ5ZcnprtWXoNqsk_asGVi4&_nc_zt=23&_nc_ht=scontent.fmnl4-6.fna&oh=03_Q7cD1gH8Vi-7upOB5gHjfRyBP6YM3VqjDdiX03t9ZRM8c6-MWg&oe=67EFBF2E" 
+      style="width:100%" class="w3-hover-opacity" alt="Sailing">
     </div>
     <button class="w3-button w3-padding-large w3-light-grey" style="margin-top:64px">LOAD MORE</button>
   </div>
 </div>
 
+ 
 
-<!-- Modal for full size images on click-->
-<div id="modal01" class="w3-modal w3-black" onclick="this.style.display='none'">
-  <span class="w3-button w3-large w3-black w3-display-topright" title="Close Modal Image"><i class="fa fa-remove"></i></span>
-  <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
-    <img id="img01" class="w3-image">
-    <p id="caption" class="w3-opacity w3-large"></p>
+<!-- Container (Contact Section) -->
+<div class="w3-content w3-container w3-padding-64" id="contact">
+  <h3 class="w3-center">WHERE I WORK</h3>
+  <p class="w3-center"><em>I'd love your feedback!</em></p>
+
+  <div class="w3-row w3-padding-32 w3-section">
+    <div class="w3-col m4 w3-container">
+      <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/e3/48/e5/river-park.jpg?w=1200&h=-1&s=1" class="w3-image w3-round" style="width:100%">
+    </div>
+    <div class="w3-col m8 w3-panel">
+      <div class="w3-large w3-margin-bottom">
+        <i class="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Chicago, US<br>
+        <i class="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Phone: +00 151515<br>
+        <i class="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Email: mail@mail.com<br>
+      </div>
+      <p>Swing by for a cup of <i class="fa fa-coffee"></i>, or leave me a note:</p>
+
+      <!-- Vue.js Form -->
+      <form @submit.prevent="submitComment">
+        <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
+          <div class="w3-half">
+            <input v-model="newComment.name" class="w3-input w3-border" type="text" placeholder="Name" required>
+          </div>
+        </div>
+        <input v-model="newComment.message" class="w3-input w3-border" type="text" placeholder="Message" required>
+        <button class="w3-button w3-black w3-right w3-section" type="submit">
+          <i class="fa fa-paper-plane"></i> SEND MESSAGE
+        </button>
+      </form>
+
+      <!-- Display Comments -->
+      <h4 class="w3-margin-top">Comments:</h4>
+      <ul>
+        <li v-for="comment in comments" :key="comment.id">
+          <strong>{{ comment.name }}</strong>: {{ comment.message }}
+        </li>
+      </ul>
+    </div>
   </div>
 </div>
-  
+
+<!-- Footer -->
+<footer class="w3-center w3-black w3-padding-64 w3-opacity w3-hover-opacity-off">
+  <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
+  <div class="w3-xlarge w3-section">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+  </div>
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+</footer>
 
 
 
-
-
-
-
-
-    
-
-    <section class='w3-container w3-padding-64' id='contact'>
-      <h2>Contact</h2>
-      <form @submit.prevent='submitComment'>
-        <input v-model='newComment' placeholder='Write a comment...'>
-        <button type='submit'>Submit</button>
-      </form>
-      <ul>
-        <li v-for='comment in comments' :key='comment.id'>{{ comment.text }}</li>
-      </ul>
-    </section>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient('https://your-supabase-url', 'your-anon-key');
+// Initialize Supabase
+const supabaseUrl = 'https://glvqdlzsvdmvplohpmgq.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsdnFkbHpzdmRtdnBsb2hwbWdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExNjAxNTYsImV4cCI6MjA1NjczNjE1Nn0.UauGe2SIarphoWwwAHweqDdV13wkK1k1lSgfFSjW0lc';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default {
-  setup() {
-    const comments = ref([]);
-    const newComment = ref('');
-
-    const fetchComments = async () => {
-      let { data } = await supabase.from('comments').select('*');
-      comments.value = data;
+  data() {
+    return {
+      newComment: { name: '', email: '', message: '' },
+      comments: []
     };
+  },
+  methods: {
+    async submitComment() {
+      const { data, error } = await supabase
+        .from('comments')
+        .insert([this.newComment]);
 
-    const submitComment = async () => {
-      await supabase.from('comments').insert([{ text: newComment.value }]);
-      newComment.value = '';
-      fetchComments();
-    };
+      if (error) {
+        console.error('Error submitting comment:', error.message);
+      } else {
+        this.comments.push(data[0]);
+        this.newComment = { name: '', email: '', message: '' }; // Reset form
+      }
+    },
+    async fetchComments() {
+      const { data, error } = await supabase
+        .from('comments')
+        .select('*')
+        .order('id', { ascending: false });
 
-    onMounted(fetchComments);
-    return { comments, newComment, submitComment };
+      if (error) {
+        console.error('Error fetching comments:', error.message);
+      } else {
+        this.comments = data;
+      }
+    }
+  },
+  mounted() {
+    this.fetchComments(); // Load comments on page load
   }
 };
 </script>
